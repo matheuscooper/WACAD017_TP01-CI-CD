1f4378e474aa034eeb77707f846fa89e1bc7893d
matheuscooper <oliveiracooper73@gmail.com>
2024-12-09 10:46:09 -0400

    Corrige erro de lint

---
+++ app.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bem vindos ao WebAcademy!!');
+});
+
+app.listen(8080, () => {
+  console.log('Rodando na porta 8080');
+});
